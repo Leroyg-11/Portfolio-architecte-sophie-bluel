4c048a105b3bebec54c213f591bf8cab52d5bd8f
Leroyg-11 <119599533+Leroyg-11@users.noreply.github.com>
2023-05-07 18:52:34 +0200

    Merci Pierro

--- Backend/script.js
+++ Backend/script.js
@@ -1,119 +1,106 @@
 //  redemander pq function Async ?
 
 async function getDataWorks() {
-    try {
-        const reponse = await fetch("http://localhost:5678/api/works/");
-        const dataWorks = await reponse.json();
-        return dataWorks
-        
-    }  catch(error){
-    console.log(error)
-    }
+  try {
+    const reponse = await fetch("http://localhost:5678/api/works/");
+    const dataWorks = await reponse.json();
+    return dataWorks;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 async function getDataCategory() {
-    try {
-        const reponse = await fetch("http://localhost:5678/api/categories/");
-        const DataCategory = await reponse.json();
-        return DataCategory
-        
-    }  catch(error){
-    console.log(error)
-    }
+  try {
+    const reponse = await fetch("http://localhost:5678/api/categories/");
+    const DataCategory = await reponse.json();
+    return DataCategory;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 async function init() {
-    const dataWorks = await getDataWorks()
-    console.log(dataWorks)
-    console.log("coucou")
+  const dataWorks = await getDataWorks();
+  console.log(dataWorks);
+  console.log("coucou");
 
-    const dataCategory = await getDataCategory()
-    // console.log(dataCategory)
+  const dataCategory = await getDataCategory();
+  // console.log(dataCategory)
 
-    function genererFiltre(dataCategory){   
-        for (let i = 0; i < dataCategory.length; i++) {
-            const container = document.getElementById("filters")
-            container.innerHTML += `<div selector="${dataCategory[i].id}" class="filter ${dataCategory[i].id} ${dataCategory[i].name}" id="${dataCategory[i].id}"> ${dataCategory[i].name} </div>`
-
-        }
+  function genererFiltre(dataCategory) {
+    for (let i = 0; i < dataCategory.length; i++) {
+      const container = document.getElementById("filters");
+      container.innerHTML += `<div selector="${dataCategory[i].id}" class="filter ${dataCategory[i].id} ${dataCategory[i].name}" id="${dataCategory[i].id}"> ${dataCategory[i].name} </div>`;
     }
-    genererFiltre(dataCategory);
+  }
+  genererFiltre(dataCategory);
 
-    function genererDataWorks(dataWorks){
-        for (let i = 0; i < dataWorks.length; i++) {
+  function genererDataWorks(dataWorks) {
+    for (let i = 0; i < dataWorks.length; i++) {
+      const figure = dataWorks[i];
 
-            const figure = dataWorks[i]
+      const sectionGallery = document.querySelector(".gallery");
 
-            const sectionGallery = document.querySelector(".gallery")
+      const workElement = document.createElement("figure");
 
-            const workElement = document.createElement("figure")
+      const imageElement = document.createElement("img");
 
-            const imageElement = document.createElement("img");
-            
-            imageElement.src = figure.imageUrl
-            const nomElement = document.createElement("p");
-            nomElement.innerText = figure.title;
+      imageElement.src = figure.imageUrl;
+      const nomElement = document.createElement("p");
+      nomElement.innerText = figure.title;
 
-            // On rattache la balise figure a la section gallery
-            sectionGallery.appendChild(workElement);
-            // On rattache l’image à workElement (la balise figure)
-            workElement.appendChild(imageElement);
-            workElement.appendChild(nomElement);
-        }
+      // On rattache la balise figure a la section gallery
+      sectionGallery.appendChild(workElement);
+      // On rattache l’image à workElement (la balise figure)
+      workElement.appendChild(imageElement);
+      workElement.appendChild(nomElement);
     }
-    genererDataWorks(dataWorks);
+  }
+  genererDataWorks(dataWorks);
 
-    // gestion des clicks filtres
+  // gestion des clicks filtres
 
-    const btnAll = document.querySelector(".filter_All")
-    // console.log(btnAll)
-    const btnOne = document.querySelector('div[selector="1"]')
-    // console.log(btnOne)
-    const btnTwo = document.querySelector('div[selector="2"]')
-    // console.log(btnTwo)
-    const btnThree = document.querySelector('div[selector="3"]')
-    // console.log(btnThree)
+  const btnAll = document.querySelector(".filter_All");
+  // console.log(btnAll)
+  const btnOne = document.querySelector('div[selector="1"]');
+  // console.log(btnOne)
+  const btnTwo = document.querySelector('div[selector="2"]');
+  // console.log(btnTwo)
+  const btnThree = document.querySelector('div[selector="3"]');
+  // console.log(btnThree)
 
-    btnAll.addEventListener("click", function (){
-        const trier = dataWorks.filter(function (dataWork){
-            return dataWork ;
-        });
-        document.querySelector("#gallery").innerHTML = "";
-        genererDataWorks(trier);
-
+  btnAll.addEventListener("click", function () {
+    const trier = dataWorks.filter(function (dataWork) {
+      return dataWork;
     });
+    document.querySelector("#gallery").innerHTML = "";
+    genererDataWorks(trier);
+  });
 
-    btnOne.addEventListener("click", function (){
-        const trier = dataWorks.filter(function (dataWork){
-            return dataWork.categoryId == 1;
-        });
-        document.querySelector("#gallery").innerHTML = "";
-        genererDataWorks(trier);
-
+  btnOne.addEventListener("click", function () {
+    const trier = dataWorks.filter(function (dataWork) {
+      return dataWork.categoryId == 1;
     });
+    document.querySelector("#gallery").innerHTML = "";
+    genererDataWorks(trier);
+  });
 
-    btnTwo.addEventListener("click", function (){
-        const trier = dataWorks.filter(function (dataWork){
-            return dataWork.categoryId == 2;
-        });
-        document.querySelector("#gallery").innerHTML = "";
-        genererDataWorks(trier);
-
+  btnTwo.addEventListener("click", function () {
+    const trier = dataWorks.filter(function (dataWork) {
+      return dataWork.categoryId == 2;
     });
+    document.querySelector("#gallery").innerHTML = "";
+    genererDataWorks(trier);
+  });
 
-    btnThree.addEventListener("click", function (){
-        const trier = dataWorks.filter(function (dataWork){
-            return dataWork.categoryId == 3;
-        });
-        document.querySelector("#gallery").innerHTML = "";
-        genererDataWorks(trier);
+  btnThree.addEventListener("click", function () {
+    const trier = dataWorks.filter(function (dataWork) {
+      return dataWork.categoryId == 3;
     });
-
-
+    document.querySelector("#gallery").innerHTML = "";
+    genererDataWorks(trier);
+  });
 }
 
-init()
-
-
-
-
+init();
--- Backend/script_log.js
+++ Backend/script_log.js
@@ -1,50 +1,26 @@
+const form = document.querySelector(".container_log");
+const error = document.querySelector(".form-error");
 
+async function postUser({ form_email, form_password}) {
+  
+  const body = {
+    email: form_email,
+    password: form_password,
+  };
 
-get("http://localhost:5678/api/users/login");
-
-
-
-// // Les informations d'identification de l'utilisateur
-// var email = 'sophie.bluel@test.tld';
-// var password = 'S0phie';
-
-
-// // Les données à envoyer dans la requête POST
-// var requestData = {
-//   email: email,
-//   password: password
-// };
-
-// // Configuration de la requête
-// var requestOptions = {
-//   method: 'POST',
-//   headers: {
-//     'Content-Type': 'application/json'
-//   },
-//   body: JSON.stringify(requestData)
-// };
-
-// console.log(requestOptions)
-
-// // // Effectuer la requête POST vers l'endpoint /users/login
-// // fetch('http://localhost:5678/api/', requestOptions)
-// //   .then(function(response) {
-// //     if (response.ok) {
-// //       return response.json();
-// //     } else {
-// //       throw new Error('Erreur lors de la requête : ' + response.status);
-// //     }
-// //   })
-// //   .then(function(data) {
-// //     // La requête a réussi, récupère les informations de la réponse
-// //     var userId = data.userId;
-// //     var token = data.token;
-// //     console.log('Utilisateur connecté avec succès. ID utilisateur : ' + userId + ', Jeton : ' + token);
-// //   })
-// //   .catch(function(error) {
-// //     // La requête a échoué, gère l'erreur
-// //     console.error('Erreur lors de la connexion utilisateur : ' + error.message);
-// //   });
+  try {
+    const response = await fetch("http://localhost:5678/api/users/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(body),
+    });
+    console.log(response)
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 
 
@@ -52,8 +28,22 @@ get("http://localhost:5678/api/users/login");
 
 
 
+function handleSubmit(event) {
+  event.preventDefault();
 
-// {
-//   "userId": 1,
-//   "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY4MzIxMTM5OCwiZXhwIjoxNjgzMjk3Nzk4fQ.c_oeA279imzqZYFYtNsQLHYG1Hsod95wgKC-KgGeI-s"
-// }
+  const form_email = event.target[0].value;
+  const form_password = event.target[1].value;
+
+  if (!form_password) {
+    error.innerHTML = "Veuillez entrer un mot de passe valide";
+    return;
+  }
+
+  postUser({form_email, form_password});
+
+  // Retrieve inputs value from form event (srcElement, value)
+
+  // Use thoses values to call the backend login api
+}
+
+form.addEventListener("submit", handleSubmit);
