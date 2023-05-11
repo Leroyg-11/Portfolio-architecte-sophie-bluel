0b85ce64fd48f5c769c8393c320b61cd1a88e8e1
Leroyg-11 <119599533+Leroyg-11@users.noreply.github.com>
2023-05-11 15:50:23 +0200

    GetToken in local Storage OK

--- Backend/script_log1.js
+++ Backend/script_log1.js
@@ -1,14 +1,15 @@
 const form = document.querySelector(".container_log");
 const error = document.querySelector(".form_error")
+// const url = await fetch("http://localhost:5678/api/users/login")
 
-console.log(form)
 
 async function postUser(form_mail, form_password){
     const body = {
         email: form_mail,
         password: form_password,
-      }
+    };
 
+ 
     try{
         const response = await fetch("http://localhost:5678/api/users/login", {
         method: "POST",
@@ -16,31 +17,53 @@ async function postUser(form_mail, form_password){
             "Content-Type": "application/json;charset=utf-8",
         },
         body: JSON.stringify(body),
+        }).then((getToken)=>getToken.json())
+        .then((dataToken)=> {
+            tokenAuth(dataToken.userId, dataToken.token)
+            // document.location = "index.html"
+            localStorage.setItem("token", dataToken.token)
+            localStorage.setItem("userId", dataToken.userId)
         });
-        document.location = "index.html"
+        // document.location = "index.html"
+       
+        
+        
+    } catch(error){
+        console.log(error);
+    };
 
 
-    }  catch(error){
-        console.log(error)
-        error.innerHTML ="Veuillez entrer une adresse mail et un mot de passe valide"
-
-    }
+};
 
 
 
-}
-
-function handleSubmit(event){
+async function handleSubmit(event){
     event.preventDefault();
 
     const form_mail = event.target[0].value
     const form_password = event.target[1].value
 
-    postUser(form_mail, form_password)
+    if (!form_password || !form_mail) {
+        error.innerHTML = "Veuillez entrer un mot de passe valide";
+        return;
+      }
+
+    await postUser(form_mail, form_password)
+
+    
+};
 
 
+function tokenAuth(userId, token){
+    userId = this.userId;
+    token = this.token
     
 }
 
 
-form.addEventListener("submit", handleSubmit)
+
+
+
+
+
+form.addEventListener("submit", handleSubmit);
